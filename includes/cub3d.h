5b0ddf92fe8f3cc8ad48e2133be3ef7909490457
Odys <tseaspap1@gmail.com>
2026-05-16 18:08:04 +0200

    Worked for 5 mins

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -2,5 +2,18 @@
 # define CUB3D_H
 # include "../libft/libft.h"
 # include "../GNL/get_next_line.h"
+# include <stdbool.h>
 
+
+
+/***********************************************************
+ *                         Parsing                          *
+ ***********************************************************/
+
+typedef struct	s_elem
+{
+	char	*elements[6];
+	char	*paths[4];
+
+}		t_elem;
 #endif
---
+++ sources/parsing/element_validation.c
@@ -0,0 +1,7 @@
+//This file will contain the validation of the N S W E F C elements
+//We will play with gnl open and split in order to get those values and
+//compare them with the accepted values
+#include "../../includes/cub3d.h"
+
+
+

b7ec56a7bd63c3d07b713a462e4fc53c58ba9eb4
opapouts <opapouts@f4r13s3.paris.42.school>
2026-05-19 22:02:37 +0200

    Testing github email modifications

--- sources/error_handling/custom_functions.c
+++ sources/error_handling/custom_functions.c
@@ -9,7 +9,7 @@
 /*   Updated: 2026/05/19 20:16:03 by opapouts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//Testing github email modifications
 #include "../../includes/cub3d.h"
 
 void	custom_error(char *msg)
